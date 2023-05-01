96646112651b4d19b996294689c610ed711ffd09
Emmanuel Aforo <reddycore14@gmail.com>
2023-05-01 20:56:18 +0000

    Updated

--- js/main.js
+++ js/main.js
@@ -1,23 +1,36 @@
-$('.navTrigger').click(function () {
-    $(this).toggleClass('active');
-    console.log("Clicked menu");
-    $("#mainListDiv").toggleClass("show_list");
-    $("#mainListDiv").fadeIn();
+// $('.navTrigger').click(function () {
+//     $(this).toggleClass('active');
+//     console.log("Clicked menu");
+//     $("#mainListDiv").toggleClass("show_list");
+//     $("#mainListDiv").fadeIn();
 
-});
+// });
 
-$(document).ready(function(){
-    $(window).scroll(function(){
-        var scroll = $(window).scrollTop();
-        if (scroll > 50) {
-          $(".main_list a").css("color" , "#0a0aa8");
-        }
+// $(document).ready(function(){
+//     $(window).scroll(function(){
+//         var scroll = $(window).scrollTop();
+//         if (scroll > 50) {
+//           $(".main_list a").css("color" , "#0a0aa8");
+//         }
    
-        else{
-            $(".main_list a").css("color" , "#fff");  	
-        }
-    })
-  })
+//         else{
+//             $(".main_list a").css("color" , "#fff");  	
+//         }
+//     })
+//   })
+
 
 
   
+$(document).ready(function() {
+    $("#card-slider").owlCarousel({
+        items : 3,
+        itemsDesktop:[1199,3],
+        itemsDesktopSmall:[980,2],
+        itemsMobile : [600,1],
+        navigation:true,
+        navigationText:["",""],
+        pagination:true,
+        autoPlay:true
+    });
+});
